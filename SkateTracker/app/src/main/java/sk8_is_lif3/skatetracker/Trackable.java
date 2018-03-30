70c8dd3b2f9af6103b53d9fd426ce73e3d0158c2
PKWouters <PKWouters@student.fullsail.edu>
2018-03-30 10:09:27 -0400

    Updated Trackable

--- SkateTracker/app/src/main/java/sk8_is_lif3/skatetracker/Trackable.java
+++ SkateTracker/app/src/main/java/sk8_is_lif3/skatetracker/Trackable.java
@@ -4,12 +4,13 @@ public class Trackable {
 
     private boolean _isTracking = false;
 
-    private int _trackerStarted = 0, _trackerEnded = 0;
+    private int _trackerStarted, _trackerEnded;
 
-    private int _trackedHours = 0, _trackedMinutes = 0, _trackedSeconds = 0;
+    private int _trackedHours, _trackedMinutes, _trackedSeconds;
 
     public Trackable(){
-
+        _trackerStarted = _trackerEnded = 0;
+        _trackedSeconds = _trackedHours = _trackedMinutes = 0;
     }
 
     public double GetSecondsTracked(){ return _trackedSeconds; }
@@ -18,22 +19,26 @@ public class Trackable {
     public boolean IsTracking(){ return _isTracking; }
 
     public void StartTracking(){
-        _trackerStarted = (int)(System.currentTimeMillis());
-        if(!_isTracking)
+        if(!_isTracking) {
             _isTracking = true;
+            if(_trackerStarted == 0)
+                _trackerStarted = (int) (System.currentTimeMillis());
+            else
+                _trackerStarted += (int)(System.currentTimeMillis()) - _trackerEnded;
+        }
     }
 
     public void PauseTracking(){
-        if(_isTracking)
+        if(_isTracking) {
             _isTracking = false;
-        _trackerEnded = (int)(System.currentTimeMillis());
-        _trackedSeconds = (_trackerEnded - _trackerStarted) / 1000;
-
-        //Convert Values Over Accordingly
-        _trackedMinutes = _trackedSeconds / 60;
-        _trackedHours = _trackedMinutes / 60;
-        _trackedMinutes = _trackedMinutes % 60;
-        _trackedSeconds = _trackedSeconds % 60;
+            _trackerEnded = (int) (System.currentTimeMillis());
+            _trackedSeconds += (int)((_trackerEnded - _trackerStarted) / 1000.0);
+            //Convert Values Over Accordingly
+            _trackedMinutes = _trackedSeconds / 60;
+            _trackedHours = _trackedMinutes / 60;
+            _trackedMinutes = _trackedMinutes % 60;
+            _trackedSeconds = _trackedSeconds % 60;
+        }
     }
 
     public void ResetTracking(){
@@ -45,11 +50,22 @@ public class Trackable {
         _trackedHours = 0;
     }
 
-    public int EllapsedTime(){
+    public String EllapsedTime(){
         if(_isTracking) {
-            int now = (int) (System.currentTimeMillis());
-            return (now - _trackerStarted) / 1000;
+            int now = (int)(System.currentTimeMillis());
+            _trackedSeconds = (int)((now - _trackerStarted) / 1000.0);
+            _trackedMinutes = _trackedSeconds / 60;
+            _trackedHours = _trackedMinutes / 60;
+            _trackedMinutes = _trackedMinutes % 60;
+            _trackedSeconds = _trackedSeconds % 60;
+            //return String.format("%d", _trackerStarted);
+        }else{
+            _trackedSeconds = (int)((_trackerEnded - _trackerStarted) / 1000.0);
+            _trackedMinutes = _trackedSeconds / 60;
+            _trackedHours = _trackedMinutes / 60;
+            _trackedMinutes = _trackedMinutes % 60;
+            _trackedSeconds = _trackedSeconds % 60;
         }
-        return _trackerStarted;
+        return String.format("%d::%d::%d", _trackedHours, _trackedMinutes, _trackedSeconds);
     }
 }
