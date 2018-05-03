7b386160a1abe19b8b09a559dcf8ca045d925118
PKWouters <PKWouters@student.fullsail.edu>
2018-05-03 14:55:00 -0400

    Started SKATEMode

---
+++ SkateTracker/app/src/main/java/sk8_is_lif3/skatetracker/SkateHome.java
@@ -0,0 +1,51 @@
+package sk8_is_lif3.skatetracker;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SkateHome extends Fragment {
+
+    public SkateHome() {
+        // Required empty public constructor
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_skate_home, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+    }
+
+}
