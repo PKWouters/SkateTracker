5b0452d4bc066d0462ff5b5fc02124682a87a45a
PKWouters <PKWouters@student.fullsail.edu>
2018-05-03 14:59:07 -0400

    Added Additional Files

---
+++ SkateTracker/app/src/main/java/sk8_is_lif3/skatetracker/SkateSetup.java
@@ -0,0 +1,13 @@
+package sk8_is_lif3.skatetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SkateSetup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skate_setup);
+    }
+}
