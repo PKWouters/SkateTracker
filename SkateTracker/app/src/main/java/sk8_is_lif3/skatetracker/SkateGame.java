f6ef80e84a91154ac9fa6404ae5b767f5225a072
PKWouters <PKWouters@student.fullsail.edu>
2018-05-08 13:29:02 -0400

    Set Color based on Position in game

--- SkateTracker/app/src/main/java/sk8_is_lif3/skatetracker/SkateGame.java
+++ SkateTracker/app/src/main/java/sk8_is_lif3/skatetracker/SkateGame.java
@@ -30,152 +30,174 @@ public class SkateGame extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skate_game);
 
-        int gameMode = 0;
+        //---INTENT STUFF HERE---//
+
+
+
+        //-----------------------//
+
+        final int gameMode = 0;
         offPlayer = 1;
         currentPlayer = 1;
 
         landBtn = (ImageButton)findViewById(R.id.landedButton);
         failBtn = (ImageButton)findViewById(R.id.failedButton);
 
-        UpdateCards();
+        UpdateCards(gameMode);
 
-        landBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(playerOneScore < 5 && playerTwoScore < 5) {
-                    if (currentPlayer == 1) {
-                        currentPlayer = 2;
-                        if (offPlayer == 2)
-                            roundNum++;
-                    } else if (currentPlayer == 2) {
-                        currentPlayer = 1;
-                        if (offPlayer == 1)
-                            roundNum++;
-                    }
-                    UpdateCards();
-                }
-            }
-        });
-
-        failBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(playerOneScore < 5 && playerTwoScore < 5){
-                    if(offPlayer == 1){
-                        if(currentPlayer == 1){
-                            currentPlayer = 2;
-                            offPlayer = 2;
-                        }else if(currentPlayer == 2){
-                            playerTwoScore++;
-                            roundNum++;
-                            TextView letterToChange;
-                            TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.p2Card));
-                            switch (playerTwoScore){
-                                case (1):
-                                    letterToChange = findViewById(R.id.s_TextP2);
-                                    letterToChange.setTextSize(70);
-                                    letterToChange.setTextColor(Color.WHITE);
-                                    letterToChange.setTypeface(null, Typeface.BOLD);
-                                    break;
-                                case (2):
-                                    letterToChange = findViewById(R.id.k_TextP2);
-                                    letterToChange.setTextSize(70);
-                                    letterToChange.setTextColor(Color.WHITE);
-                                    letterToChange.setTypeface(null, Typeface.BOLD);
-                                    break;
-                                case (3):
-                                    letterToChange = findViewById(R.id.a_TextP2);
-                                    letterToChange.setTextSize(70);
-                                    letterToChange.setTextColor(Color.WHITE);
-                                    letterToChange.setTypeface(null, Typeface.BOLD);
-                                    break;
-                                case (4):
-                                    letterToChange = findViewById(R.id.t_TextP2);
-                                    letterToChange.setTextSize(70);
-                                    letterToChange.setTextColor(Color.WHITE);
-                                    letterToChange.setTypeface(null, Typeface.BOLD);
-                                    break;
-                                case (5):
-                                    letterToChange = findViewById(R.id.e_TextP2);
-                                    letterToChange.setTextSize(70);
-                                    letterToChange.setTextColor(Color.WHITE);
-                                    letterToChange.setTypeface(null, Typeface.BOLD);
-                                    break;
-                            }
-
-                            currentPlayer = 1;
-                        }
-                    }else if(offPlayer == 2) {
+        if(gameMode == 0) { //---CLASSIC SKATE---//
+            landBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (playerOneScore < 5 && playerTwoScore < 5) {
                         if (currentPlayer == 1) {
-                            TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.p1Card));
-                            playerOneScore++;
-                            roundNum++;
-                            TextView letterToChange;
-                            switch (playerOneScore) {
-                                case (1):
-                                    letterToChange = findViewById(R.id.s_Text);
-                                    letterToChange.setTextSize(70);
-                                    letterToChange.setTextColor(Color.WHITE);
-                                    letterToChange.setTypeface(null, Typeface.BOLD);
-                                    break;
-                                case (2):
-                                    letterToChange = findViewById(R.id.k_Text);
-                                    letterToChange.setTextSize(70);
-                                    letterToChange.setTextColor(Color.WHITE);
-                                    letterToChange.setTypeface(null, Typeface.BOLD);
-                                    break;
-                                case (3):
-                                    letterToChange = findViewById(R.id.a_Text);
-                                    letterToChange.setTextSize(70);
-                                    letterToChange.setTextColor(Color.WHITE);
-                                    letterToChange.setTypeface(null, Typeface.BOLD);
-                                    break;
-                                case (4):
-                                    letterToChange = findViewById(R.id.t_Text);
-                                    letterToChange.setTextSize(70);
-                                    letterToChange.setTextColor(Color.WHITE);
-                                    letterToChange.setTypeface(null, Typeface.BOLD);
-                                    break;
-                                case (5):
-                                    letterToChange = findViewById(R.id.e_Text);
-                                    letterToChange.setTextSize(70);
-                                    letterToChange.setTextColor(Color.WHITE);
-                                    letterToChange.setTypeface(null, Typeface.BOLD);
-                                    break;
-                            }
                             currentPlayer = 2;
+                            if (offPlayer == 2)
+                                roundNum++;
                         } else if (currentPlayer == 2) {
                             currentPlayer = 1;
-                            offPlayer = 1;
+                            if (offPlayer == 1)
+                                roundNum++;
                         }
+                        UpdateCards(gameMode);
                     }
-                    UpdateCards();
                 }
-            }
-        });
+            });
+            failBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (playerOneScore < 5 && playerTwoScore < 5) {
+                        if (offPlayer == 1) {
+                            if (currentPlayer == 1) {
+                                currentPlayer = 2;
+                                offPlayer = 2;
+                            } else if (currentPlayer == 2) {
+                                playerTwoScore++;
+                                roundNum++;
+                                TextView letterToChange;
+                                TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.p2Card));
+                                switch (playerTwoScore) {
+                                    case (1):
+                                        letterToChange = findViewById(R.id.s_TextP2);
+                                        letterToChange.setTextSize(70);
+                                        letterToChange.setTextColor(Color.WHITE);
+                                        letterToChange.setTypeface(null, Typeface.BOLD);
+                                        break;
+                                    case (2):
+                                        letterToChange = findViewById(R.id.k_TextP2);
+                                        letterToChange.setTextSize(70);
+                                        letterToChange.setTextColor(Color.WHITE);
+                                        letterToChange.setTypeface(null, Typeface.BOLD);
+                                        break;
+                                    case (3):
+                                        letterToChange = findViewById(R.id.a_TextP2);
+                                        letterToChange.setTextSize(70);
+                                        letterToChange.setTextColor(Color.WHITE);
+                                        letterToChange.setTypeface(null, Typeface.BOLD);
+                                        break;
+                                    case (4):
+                                        letterToChange = findViewById(R.id.t_TextP2);
+                                        letterToChange.setTextSize(70);
+                                        letterToChange.setTextColor(Color.WHITE);
+                                        letterToChange.setTypeface(null, Typeface.BOLD);
+                                        break;
+                                    case (5):
+                                        letterToChange = findViewById(R.id.e_TextP2);
+                                        letterToChange.setTextSize(70);
+                                        letterToChange.setTextColor(Color.WHITE);
+                                        letterToChange.setTypeface(null, Typeface.BOLD);
+                                        break;
+                                }
+
+                                currentPlayer = 1;
+                            }
+                        } else if (offPlayer == 2) {
+                            if (currentPlayer == 1) {
+                                TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.p1Card));
+                                playerOneScore++;
+                                roundNum++;
+                                TextView letterToChange;
+                                switch (playerOneScore) {
+                                    case (1):
+                                        letterToChange = findViewById(R.id.s_Text);
+                                        letterToChange.setTextSize(70);
+                                        letterToChange.setTextColor(Color.WHITE);
+                                        letterToChange.setTypeface(null, Typeface.BOLD);
+                                        break;
+                                    case (2):
+                                        letterToChange = findViewById(R.id.k_Text);
+                                        letterToChange.setTextSize(70);
+                                        letterToChange.setTextColor(Color.WHITE);
+                                        letterToChange.setTypeface(null, Typeface.BOLD);
+                                        break;
+                                    case (3):
+                                        letterToChange = findViewById(R.id.a_Text);
+                                        letterToChange.setTextSize(70);
+                                        letterToChange.setTextColor(Color.WHITE);
+                                        letterToChange.setTypeface(null, Typeface.BOLD);
+                                        break;
+                                    case (4):
+                                        letterToChange = findViewById(R.id.t_Text);
+                                        letterToChange.setTextSize(70);
+                                        letterToChange.setTextColor(Color.WHITE);
+                                        letterToChange.setTypeface(null, Typeface.BOLD);
+                                        break;
+                                    case (5):
+                                        letterToChange = findViewById(R.id.e_Text);
+                                        letterToChange.setTextSize(70);
+                                        letterToChange.setTextColor(Color.WHITE);
+                                        letterToChange.setTypeface(null, Typeface.BOLD);
+                                        break;
+                                }
+                                currentPlayer = 2;
+                            } else if (currentPlayer == 2) {
+                                currentPlayer = 1;
+                                offPlayer = 1;
+                            }
+                        }
+                        UpdateCards(gameMode);
+                    }
+                }
+            });
+        }else if(gameMode == 1){ //---PRE-GEN SKATE---//
+
+        }
 
     }
-    private void UpdateCards(){
-        if(currentPlayer == 1){
-            TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.p1Card));
-            CardView p1Card = findViewById(R.id.p1Card);
-            p1Card.setCardBackgroundColor(getResources().getColor(R.color.colorAccent));
+    private void UpdateCards(int gameMode){
 
+        if(gameMode == 0) {
+            if(currentPlayer == 1){
+                TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.p1Card));
+                CardView p1Card = findViewById(R.id.p1Card);
+                p1Card.setCardBackgroundColor(getResources().getColor(R.color.colorAccent));
+                if(offPlayer == 2) {
+                    TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.p2Card));
+                    CardView p2Card = findViewById(R.id.p2Card);
+                    p2Card.setCardBackgroundColor(getResources().getColor(R.color.colorAccentDark));
+                }else {
+                    TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.p2Card));
+                    CardView p2Card = findViewById(R.id.p2Card);
+                    p2Card.setCardBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                }
 
-            TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.p2Card));
-            CardView p2Card = findViewById(R.id.p2Card);
-            p2Card.setCardBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+            }else if(currentPlayer == 2){
+                TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.p2Card));
+                CardView p2Card = findViewById(R.id.p2Card);
+                p2Card.setCardBackgroundColor(getResources().getColor(R.color.colorAccent));
 
-        }else if(currentPlayer == 2){
-            TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.p2Card));
-            CardView p2Card = findViewById(R.id.p2Card);
-            p2Card.setCardBackgroundColor(getResources().getColor(R.color.colorAccent));
-
-            TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.p1Card));
-            CardView p1Card = findViewById(R.id.p1Card);
-            p1Card.setCardBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                if(offPlayer == 1) {
+                    TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.p1Card));
+                    CardView p1Card = findViewById(R.id.p1Card);
+                    p1Card.setCardBackgroundColor(getResources().getColor(R.color.colorAccentDark));
+                }else {
+                    TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.p1Card));
+                    CardView p1Card = findViewById(R.id.p1Card);
+                    p1Card.setCardBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                }
+            }
+                TextView roundNumView = findViewById(R.id.roundTrickText);
+                roundNumView.setText("Round: " + roundNum);
         }
-        TextView roundNumView = findViewById(R.id.roundTrickText);
-        roundNumView.setText("Round: " + roundNum);
     }
 }
