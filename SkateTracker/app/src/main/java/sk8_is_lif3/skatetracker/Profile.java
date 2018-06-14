2121461048a5317c5d6e86474fe8efc7a70f276a
chico medeiros <grampacupcake@chicos-MacBook-Pro.local>
2018-06-14 17:17:32 -0400

    update profile

--- SkateTracker/app/src/main/java/sk8_is_lif3/skatetracker/Profile.java
+++ SkateTracker/app/src/main/java/sk8_is_lif3/skatetracker/Profile.java
@@ -11,19 +11,34 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import sk8_is_lif3.skatetracker.transitions.Achievement;
 
+
+
 public class Profile extends Fragment {
+
+    FirebaseFirestore db;
+    FirebaseUser user;
+    private TextView tv;
+    private String holder = "fghjfghj";
 
     public Profile() {
         // Required empty public constructor
@@ -32,36 +47,18 @@ public class Profile extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
-        Button signOut = view.findViewById(R.id.signOut);
-        signOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(FirebaseAuth.getInstance().getCurrentUser() != null){
-                    AuthUI.getInstance()
-                            .signOut(getActivity())
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    // ...
-                                    Toast.makeText(getContext(), "Signed Out", Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                }
-            }
-        });
-        Button viewAchievements = view.findViewById(R.id.achieveButton);
-        viewAchievements.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), AchievementActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                getActivity().startActivity(intent);
-            }
-        });
         Toolbar toolbar = (Toolbar) getView().findViewById(R.id.toolbar);
         toolbar.setTitle("Profile");
         AppCompatActivity activity = (AppCompatActivity)getActivity();
         activity.setSupportActionBar(toolbar);
-
+        activity.getSupportActionBar().setDisplayShowTitleEnabled(true);
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        activity.getSupportActionBar().setDisplayShowHomeEnabled(true);
+        activity.getSupportActionBar().setHomeButtonEnabled(true);
+        setHasOptionsMenu(true);
+        if(user != null) {
+            tv.setText(user.getDisplayName());
+        }
     }
 
     @Override
@@ -71,17 +68,56 @@ public class Profile extends Fragment {
         setExitTransition(TransitionInflater.from(getActivity()).inflateTransition(android.R.transition.fade));
         setReturnTransition(TransitionInflater.from(getActivity()).inflateTransition(android.R.transition.fade));
         setReenterTransition(TransitionInflater.from(getActivity()).inflateTransition(android.R.transition.fade));
+
+        db = FirebaseFirestore.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        tv = (TextView)v.findViewById(R.id.welcomename);
+        return v;
     }
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
 
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_profile, menu);
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.the_achivements:
+
+                        Intent intent = new Intent(getContext(), AchievementActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                        getActivity().startActivity(intent);
+                        break;
+            case R.id.the_sign_out:
+                        if(FirebaseAuth.getInstance().getCurrentUser() != null){
+                            AuthUI.getInstance()
+                                    .signOut(getActivity())
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            // ...
+                                            Toast.makeText(getContext(), "Signed Out", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                        }
+                        break;
+        }
+        return true;
     }
 }
